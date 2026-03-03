cf35e68b03963afc190474d6fbdb6c41ae350009
Daniel Abbott <145143890+dabbott05@users.noreply.github.com>
2026-03-02 22:29:52 -0600

    Update file paths and keywords in html_verifier.py

--- scripts/html_verifier.py
+++ scripts/html_verifier.py
@@ -1,5 +1,6 @@
 # REPLACE THE * WITH YOUR LOCAL PI's USERNAME
 
+import time
 import os
 import requests
 import urllib3
@@ -8,17 +9,13 @@ import datetime
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Daily logging | I have a cron job set to run at 11:50pm every night to run this script .
-# The time it takes for this script to complete is longer than 10 minutes so we subtract 
-# 6 hours from the time to ensure there are no errors with saving data to the wrong file .
 adjusted_time = datetime.datetime.now() - datetime.timedelta(hours=6)
 today = adjusted_time.strftime("%Y-%m-%d")
-INPUT_FILE = f"/home/*/scam_logs/targets_{today}.txt"
-OUTPUT_FILE = f"/home/*/scam_logs/confirmed_{today}.txt"
+INPUT_FILE = f"/home/lild/scam_logs/targets_{today}.txt"
+OUTPUT_FILE = f"/home/lild/scam_logs/confirmed_{today}.txt"
 
-# Create your own keywords !
-CRYPTO_TERMS = ['bitcoin', 'crypto']
-HYIP_TERMS = ['daily roi', 'investment plan']
+CRYPTO_TERMS = ['bitcoin', 'usdt', 'ethereum', 'crypto', 'wallet address', 'deposit amount', 'withdraw']
+HYIP_TERMS = ['daily roi', 'investment plan', 'standard plan', 'premium plan', 'daily return', 'silver plan', 'gold plan', 'mining plan', 'referral commission', 'minimum deposit', 'task center', 'frozen amount', 'recharge']
 
 active_threats = []
 seen_urls = set()
@@ -36,6 +33,7 @@ def check_html_and_save(target):
     # Ensure URL is clean for comparison
     strict_url = target if target.startswith("http") else f"https://{target}/"
     
+    # do i even need this if its a set ??? what was i thinking will fix tmmrw
     if strict_url in seen_urls: 
         return
         
@@ -47,8 +45,7 @@ def check_html_and_save(target):
             
             crypto_hits = sum(1 for term in CRYPTO_TERMS if term in html_body)
             hyip_hits = sum(1 for term in HYIP_TERMS if term in html_body)
-
-            # the logic behing deciding which websites to return . edit this to get more or less returns .
+            
             if crypto_hits >= 1 and hyip_hits >= 2:
                 print(f"[+] NEW SCAM CONFIRMED: {strict_url}")
                 with write_lock:
@@ -78,8 +75,9 @@ if __name__ == "__main__":
         t = threading.Thread(target=check_html_and_save, args=(target,))
         threads.append(t)
         t.start()
-        
-        if len(threads) >= 20: # This is at 20 since I am using a rasberry Pi and I dont want to destroy it . If you can run 100 threads ... do it !!!
+
+        time.sleep(0.1)
+        if len(threads) >= 20:
             for t in threads: t.join()
             threads = []
             
